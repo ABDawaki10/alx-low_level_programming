430e68f8e0f6251422b9c33b8ad0bbb338a77e54
ABDawaki10 <abubakar.bashir51@gmail.com>
2022-09-09 10:40:41 -0700

    task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * Return: prints numbers from 0 to 9.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
