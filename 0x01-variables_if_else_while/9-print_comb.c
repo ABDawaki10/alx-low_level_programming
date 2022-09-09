23b90f7577667152fc5cd0bb8b63b3897b7772dc
ABDawaki10 <abubakar.bashir51@gmail.com>
2022-09-09 13:17:40 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,18 @@
 /**
  * main - checks if positive, zero or negative
  *
- * Return: zero
+ * Return: Always 0 (Success)
  */
 int main(void)
-{
-	int n;
+{	
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+	for (i = 48; i < 58; i++)
+	putchar(i);
+	if (i != 57)
+		putchar(',');
+		putchar(' ');
+
+	putchar('\n');
 	return (0);
-}	
+}
