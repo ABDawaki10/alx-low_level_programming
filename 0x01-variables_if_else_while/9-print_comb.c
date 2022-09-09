a058ec6cd4ea11be0dd147f6734aa83d28b4846f
ABDawaki10 <abubakar.bashir51@gmail.com>
2022-09-09 13:24:59 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,20 @@
 #include <stdio.h>
 
 /**
- * main - checks if positive, zero or negative
+ * main - prints numbers between 0 to 9 with commas and spaces.
  *
- * Return: Always 0 (Success)
+ * Return: ALways 0 (Success)
  */
 int main(void)
-{	
+{
 	int i;
 
 	for (i = 48; i < 58; i++)
-	putchar(i);
+		putchar(i);
 	if (i != 57)
 		putchar(',');
 		putchar(' ');
-
 	putchar('\n');
 	return (0);
 }
+
